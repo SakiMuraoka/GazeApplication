5d3f02572199d32a1713cdbe5251fa7614f4694b
SakiMuraoka <murasaki9322@gmail.com>
2020-08-01 15:15:20 +0900

    add iconButton

--- GazeApplication/SampleView.swift
+++ GazeApplication/SampleView.swift
@@ -21,29 +21,34 @@ class SampleView: UIViewController {
     let distanceFromCenter: CGFloat = 100
     
     var faceDirectionArrow: FaceDirectionArrowView!
+    
+    var iconView: IconView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //グリッド線の表示
         let gridView = GridView(frame: self.view.bounds)
-        self.view.addSubview(gridView)
+        //self.view.addSubview(gridView)
         //注視点の表示
         self.Looking = LookAtPosition(frame: self.view.bounds)
-        self.view.addSubview(self.Looking)
+        //self.view.addSubview(self.Looking)
         //注視点数値の表示
         self.windowWidth = self.view.frame.width
         self.windowHeight = self.view.frame.height
         updateLookAtPositionText(lookAt: point)
-        drawLookAtPositionLabel()
+        //drawLookAtPositionLabel()
         //瞬きの表示（右目バージョン）
         self.windowWidthCenter = self.view.bounds.width / 2
         self.eyeBlinkView = EyeBlinkView(frame: self.view.bounds)
         self.eyeBlinkView.setPosition(posx: windowWidthCenter + distanceFromCenter)
-        self.view.addSubview(self.eyeBlinkView)
+        //self.view.addSubview(self.eyeBlinkView)
         //顔の向きの矢印を表示
         self.faceDirectionArrow = FaceDirectionArrowView(frame: self.view.bounds)
-        self.view.addSubview(self.faceDirectionArrow)
+        //self.view.addSubview(self.faceDirectionArrow)
+        //iconViewの表示
+        self.iconView = IconView(frame: self.view.bounds)
+        self.view.addSubview(iconView)
     }
 }
 
