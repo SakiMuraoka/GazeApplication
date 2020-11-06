1798dfd51986a59c0fbe17bc6fd4e5051b7b7851
SakiMuraoka <murasaki9322@gmail.com>
2020-11-06 11:39:06 +0900

    EyeTrackKitでデータを記録できるように変更

--- GazeApplication/ViewController/DataController.swift
+++ GazeApplication/ViewController/DataController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import CSV
 import EyeTrackKit
+import UIKit
 
 enum DataStatus {
     case INITIALIZED
@@ -18,55 +19,84 @@ enum DataStatus {
 
 public class DataController: ObservableObject {
     @Published var status: DataStatus = .INITIALIZED
-    @Published var data: [EyeTrackInfo] = []
+    @Published var datas: [[Any]] = []
     //ファイルの場所
     let DOCUMENT_DIRECTORY_PAYH = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last!
 
     public func start() {
         self.status = .RECORDING
+        print("datacontroller start")
     }
 
-    public func add(info: EyeTrackInfo) {
+    public func add(info: EyeTrackInfo, targetPoint: CGPoint) {
         if self.status == .RECORDING {
-            self.data.append(info)
+            self.datas.append([info, targetPoint])
+        }
+    }
+    public func add(info: EyeTrackInfo, operationType: String, operationPoint: CGPoint){
+        if self.status == .RECORDING {
+            self.datas.append([info, operationType, operationPoint])
         }
     }
 
     public func stop() {
-        print("Acquired \(data.count) frames")
+        print("Acquired \(datas.count) frames")
         self.status = .FINISHED
     }
 
     public func export(name: String, coloumns: [String], rows: [[String]]) -> Void {
         let stream = OutputStream(toFileAtPath: name, append: false)!
         let csv = try! CSVWriter(stream: stream)
-
         try! csv.write(row: coloumns)
         for row in rows {
             try! csv.write(row: row)
         }
         csv.stream.close()
     }
+    
+    public let gaze_COLUMNS = ["targetPoint-x", "targetPoint-y"]
+    public let other_COLUMNS = ["operationType", "operationPoint-x", "operationPoint-y"]
 
-    public func export(name: String) -> Void {
-        let filePath = DOCUMENT_DIRECTORY_PAYH + "/" + name
+    public func export(name: String, myapp: String) -> Void {
+        let filePath = DOCUMENT_DIRECTORY_PAYH + "/" + name  + ".csv"
         let fileURL = URL(fileURLWithPath: filePath)
         let stream = OutputStream(url: fileURL, append: false)!
         let csv = try! CSVWriter(stream: stream)
-
-        try! csv.write(row: EyeTrackInfo.CSV_COLUMNS)
-        for info in data {
-            try! csv.write(row: info.toCSV)
+        if(myapp == "eyegaze" || myapp == "eye"){
+            try! csv.write(row: EyeTrackInfo.CSV_COLUMNS + gaze_COLUMNS)
+            for data in datas {
+                let info = data[0] as! EyeTrackInfo
+                let targetPoint = data[1] as! CGPoint
+                let targetPoint_ = [targetPoint.x, targetPoint.y]
+                var plusData_:[String] = []
+                for i in 0..<targetPoint_.count {
+                    plusData_.append(String(format: "%.8f", targetPoint_[i] as CVarArg))
+                }
+                let row = info.toCSV + plusData_
+                try! csv.write(row: row)
+            }
+        }else{
+            try! csv.write(row: EyeTrackInfo.CSV_COLUMNS + other_COLUMNS)
+            for data in datas {
+                let info = data[0] as! EyeTrackInfo
+                let operationType = data[1] as! String
+                let oPoint = data[2] as! CGPoint
+                let oPoint_ = [oPoint.x, oPoint.y]
+                var plusData_:[String] = [operationType]
+                for i in 0..<oPoint_.count {
+                    plusData_.append(String(format: "%.8f", oPoint_[i] as CVarArg))
+                }
+                let row = info.toCSV + plusData_
+                try! csv.write(row: row)
+            }
         }
         csv.stream.close()
+        
     }
 
     public func reset() {
-        self.data.removeAll()
+        self.datas.removeAll()
         self.status = .INITIALIZED
     }
-
-
-
 }
 
--- GazeApplication/ViewController/EyePointView_y.swift
+++ GazeApplication/ViewController/EyePointView_y.swift
@@ -8,20 +8,15 @@
 import UIKit
 import SceneKit
 import ARKit
-import WebKit
-import ARVideoKit
+import EyeTrackKit
+import SwiftUI
 
-class EyePointView_y: EyeTrackViewController_y {
-
-    var gridView: GridView!
+class EyePointView_y: EyeTrackViewController {
     
-    var leftEyeView: UIView!
-    var rightEyeView: UIView!
-    var eyePositionIndicatorView: GazePointer!
-    var eyeTrackingPositionView: UIView!
-    var eyeTargetPositionXLabel: UILabel!
-    var eyeTargetPositionYLabel: UILabel!
-    var distanceLabel: UILabel!
+    var eyeTrackController: EyeTrackController!
+    var dataController: DataController!
+    
+    var gridView: GridView!
     
     var error: Bool = true
     var errorLabel: UILabel!
@@ -33,8 +28,6 @@ class EyePointView_y: EyeTrackViewController_y {
     
     let csvModel = CsvModel()
     var participant: String? = "saki"
-    var dataLists = [[""]]
-    var frameId: Int = 0
     
     var eyeTrajectryList: [GazeTrajectory] = []
     
@@ -50,40 +43,21 @@ class EyePointView_y: EyeTrackViewController_y {
         super.viewDidLoad()
         
         gridView = GridView(frame: self.view.bounds)
-        
-        eyePositionIndicatorView = GazePointer(frame: self.gridView.bounds)
-        leftEyeView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 5))
-        rightEyeView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 5))
-        eyeTrackingPositionView = UIView(frame: CGRect(x: 0, y: 0, width: 5, height: 5))
-        eyeTargetPositionXLabel = UILabel(frame: CGRect(x: 0, y: 100, width: 40, height: 15))
-        eyeTargetPositionYLabel = UILabel(frame: CGRect(x: 60, y: 100, width: 40, height: 15))
-        distanceLabel = UILabel(frame: CGRect(x: 110, y: 100, width: 50, height: 15))
+        gridView.backgroundColor = UIColor.white
         
         errorLabel = UILabel(frame: CGRect(x: 0, y: self.view.center.y, width: self.view.bounds.width, height: 50))
         errorLabel.textAlignment = .center
         errorLabel.isHidden = false
         errorLabel.text = "顔をカメラに写してください"
         
-            //視線カーソル表示の処理（場合わけで変更可）
-            eyePositionIndicatorView.isHidden = true
-        
         eyePointTarget = TestEyePointTarget(frame: self.view.bounds)
-        eyePointTarget.center = CGPoint(x: 2*Int(interval) - offsetX, y:Int(interval)*4-offsetY)
+        eyePointTarget.center = CGPoint(x: 5*Int(interval) - offsetX, y:Int(interval)*10-offsetY)
         eyePointTarget.Resize(radius: self.view.bounds.width/30)
-        eyePointTarget.isHidden = true
+        eyePointTarget.isHidden = false
         
         self.view.addSubview(gridView)
-        self.view.addSubview(eyePositionIndicatorView)
-        self.eyePositionIndicatorView.addSubview(leftEyeView)
-        self.eyePositionIndicatorView.addSubview(rightEyeView)
-        self.eyePositionIndicatorView.addSubview(eyeTrackingPositionView)
-        self.view.addSubview(eyeTargetPositionXLabel)
-        self.view.addSubview(eyeTargetPositionYLabel)
-        self.view.addSubview(distanceLabel)
         self.view.addSubview(errorLabel)
         self.view.addSubview(eyePointTarget)
-        
-//        moveTarget(fig: eyePointTarget)
         
         if mode == 0 {
             mymode = "demo"
@@ -97,26 +71,30 @@ class EyePointView_y: EyeTrackViewController_y {
         }else{
 
         }
-
-        
+        eyeTrackController = EyeTrackController(device: EyeTrackKit.Device(type: .iPhone), smoothingRange: 10, blinkThreshold: .infinity, isHidden: false)
+        dataController = DataController()
+        self.eyeTrackController.onUpdate = { info in
+            self.errorLabel.isHidden = true
+            let targetPoint = self.eyePointTarget.layer.presentation()?.position
+            self.dataController.add(info: info!, targetPoint: targetPoint!)
+            print(info!.leftEyeDistance)
+        }
+        self.initialize(eyeTrack: eyeTrackController.eyeTrack)
+//        self.show()
     }
+    
     
     //MARK:- アラート表示
     func displayAlert(title: String, message: String){
         let alert: UIAlertController = UIAlertController(title: title, message: message, preferredStyle:  UIAlertController.Style.alert)
-        // OKボタン
-//        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler:{
-//            // ボタンが押された時の処理を書く（クロージャ実装）
-//            (action: UIAlertAction!) -> Void in
-//            print("OK")
-//            self.recordState = true
-//        })
+        
         let actionChoise1 = UIAlertAction(title: "注視なし", style: .default){
             action in
             print("注視なし")
             self.recordState = true
-            self.eyePointTarget.isHidden = false
             self.moveTarget(fig: self.eyePointTarget)
+            self.eyeTrackController.startRecord()
+            self.dataController.start()
         }
 
         let actionChoise2 = UIAlertAction(title: "注視あり", style: .default){
@@ -124,8 +102,9 @@ class EyePointView_y: EyeTrackViewController_y {
             print("注視あり")
             self.myapp = "eyegaze"
             self.recordState = true
-            self.eyePointTarget.isHidden = false
             self.moveTarget(fig: self.eyePointTarget)
+            self.eyeTrackController.startRecord()
+            self.dataController.start()
         }
         // キャンセルボタン
         let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル", style: UIAlertAction.Style.cancel, handler:{
@@ -141,34 +120,21 @@ class EyePointView_y: EyeTrackViewController_y {
         present(alert, animated: true, completion: nil)
     }
     
-    @objc func dataButtonClick(_ sender: UIButton){
-        let now = NSDate()
-        let time = timeToString(date: now as Date, mode: 1)
-        self.frameId += 1
-        let data = [eyeTrack.lookAtPosition.x, eyeTrack.lookAtPosition.y,]
-        var dataString: [String] = [String(frameId), time, mymode]
-        for i in 0..<data.count {
-            dataString.append(String(format: "%.8f", data[i]))
-        }
-        //print(dataString)
-        dataLists.append(dataString)
-    }
-    
     func moveTarget(fig: UIView) {
         //初期位置をセット
-        let i = 2
-        let j = 4
+//        let i = 5
+//        let j = 11
         let timeLimit = 120.0
-        testTargetAnimation(fig: fig, x: i, y: j)
+        testTargetAnimation(fig: fig)
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + timeLimit) {
             self.eyePointTarget.isHidden = true
             self.navigationController?.popViewController(animated: true)
         }
     }
     
-    func testTargetAnimation(fig: UIView, x: Int, y: Int){
-        var i = x
-        var j = y
+    func testTargetAnimation(fig: UIView){
+        var i = 0
+        var j = 0
         let mini = 2
         let minj = 4
         let maxi = 8
@@ -180,13 +146,13 @@ class EyePointView_y: EyeTrackViewController_y {
             UIView.animate(withDuration: 2, delay: 0, options:[.curveLinear], animations: {
                 fig.center = CGPoint(x: Int(self.interval)*i - self.offsetX, y: Int(self.interval)*j - self.offsetY)
             }, completion: { finished in
-                self.testTargetAnimation(fig: fig, x: i, y: j)
+                self.testTargetAnimation(fig: fig)
             })
         }else{
             UIView.animate(withDuration: 2, delay: 5, options:[.curveLinear], animations: {
                 fig.center = CGPoint(x: Int(self.interval)*i - self.offsetX, y: Int(self.interval)*j - self.offsetY)
             }, completion: { finished in
-                self.testTargetAnimation(fig: fig, x: i, y: j)
+                self.testTargetAnimation(fig: fig)
             })
         }
     }
@@ -203,75 +169,12 @@ class EyePointView_y: EyeTrackViewController_y {
             let now = NSDate()
             let time = timeToString(date: now as Date, mode: 0)
             let fileName = csvModel.convertConditionsToFileName(name: username, conditions: [time, myapp, mymode])
-            let data = csvModel.convertFigureListToString(dataLists: dataLists)
-            let dataRows = ["frameId", "timestamp", "lookAtPosition_x", "lookAtPosition_y", "target_x", "target_y", "face_0x","face_0y", "face_0z", "face_0w", "face_1x","face_1y", "face_1z", "face_1w", "face_2x","face_2y", "face_2z", "face_2w", "face_3x","face_3y", "face_3z", "face_3w", "right_0x","right_0y", "right_0z", "right_0w", "right_1x","right_1y", "right_1z", "right_1w", "right_2x","right_2y", "right_2z", "right_2w", "right_3x","right_3y", "right_3z", "right_3w", "left_0x","left_0y", "left_0z", "left_0w", "left_1x","left_1y", "left_1z", "left_1w", "left_2x","left_2y", "left_2z", "left_2w", "left_3x","left_3y", "left_3z", "left_3w"]
-            let rowNames = csvModel.convertDataToCSV(list: dataRows)
-            csvModel.write(fileName: fileName, rowsName: rowNames, dataList: data)
+            self.dataController.stop()
+            //                eyeTrackController.stop(finished: {_ in}, isExport: true) // export video to Photo Library
+            self.eyeTrackController.stopRecord(finished: { path in print("Video File Path: \(path)") }, isExport: false) // export video to Documents folder
+            self.dataController.export(name: fileName, myapp: self.myapp)
+            self.dataController.reset()
                 recordState = false
-        }
-    }
-
-    override func updateViewWithUpdateAnchor() {
-        if(recordState){
-            // update indicator position
-             if eyeTrack.lookAtPosition.x < -view.bounds.width/2 + eyePositionIndicatorView.frame.width {
-                 eyeTrack.lookAtPosition.x = -view.bounds.width/2 + eyePositionIndicatorView.frame.width
-             }else if eyeTrack.lookAtPosition.x > view.bounds.width/2 {
-                 eyeTrack.lookAtPosition.x = view.bounds.width/2
-             }
-            if eyeTrack.lookAtPosition.y < -view.bounds.height/2 + eyePositionIndicatorView.frame.height + 50{
-                eyeTrack.lookAtPosition.y = -view.bounds.height/2 + eyePositionIndicatorView.frame.height + 50
-            }else if eyeTrack.lookAtPosition.y > view.bounds.height/2{
-                eyeTrack.lookAtPosition.y = view.bounds.height/2
-            }
-            self.eyePositionIndicatorView.transform = CGAffineTransform(translationX: eyeTrack.lookAtPosition.x, y: eyeTrack.lookAtPosition.y)
-            if eyeTrack.lookAtPoint.x < 0 {
-                self.eyeTargetPositionXLabel.text = "0"
-            } else {
-                self.eyeTargetPositionXLabel.text = "\(Int(round(eyeTrack.lookAtPoint.x)))"
-            }
-
-            if eyeTrack.lookAtPoint.y < 0 {
-                self.eyeTargetPositionYLabel.text = "0"
-            } else {
-                self.eyeTargetPositionYLabel.text = "\(Int(round(eyeTrack.lookAtPoint.y)))"
-            }
-
-            // Update distance label value
-            self.distanceLabel.text = "\(Int(round(eyeTrack.face.getDistanceToDevice() * 100))) cm"
-            //視線カーソルの軌跡の表示
-//            if(mode == 0){
-//                let eyeTrajectry = GazeTrajectory(frame: self.gridView.bounds)
-//                eyeTrajectry.center = CGPoint(x: eyeTrack.lookAtPosition.x + view.bounds.width/2, y: eyeTrack.lookAtPosition.y + view.bounds.height/2)
-//                eyeTrajectryList.append(eyeTrajectry)
-//                if(eyeTrajectryList.count > 30){
-//                    eyeTrajectryList.first?.removeFromSuperview()
-//                    eyeTrajectryList.removeFirst()
-//                }
-//                self.view.addSubview(eyeTrajectryList[eyeTrajectryList.count - 1])
-//            }
-            let target = eyePointTarget.layer.presentation()?.position
-            let now = NSDate()
-            let time = timeToString(date: now as Date, mode: 1)
-            self.frameId += 1
-            let face_t = eyeTrack.face.transform.columns
-            let right_t = eyeTrack.face.rightEye.node.simdTransform.columns
-            let left_t = eyeTrack.face.leftEye.node.simdTransform.columns
-            let data = [eyePositionIndicatorView.center.x + eyeTrack.lookAtPosition.x, eyePositionIndicatorView.center.y + eyeTrack.lookAtPosition.y ,target!.x, target!.y, face_t.0.x, face_t.0.y, face_t.0.z, face_t.0.w,face_t.1.x, face_t.1.y, face_t.1.z, face_t.1.w, face_t.2.x, face_t.2.y, face_t.2.z, face_t.2.w, face_t.3.x, face_t.3.y, face_t.3.z, face_t.3.w, right_t.0.x, right_t.0.y, right_t.0.z, right_t.0.w,right_t.1.x, right_t.1.y, right_t.1.z, right_t.1.w, right_t.2.x, right_t.2.y, right_t.2.z, right_t.2.w, right_t.3.x, right_t.3.y, right_t.3.z, right_t.3.w, left_t.0.x, left_t.0.y, left_t.0.z, left_t.0.w,left_t.1.x, left_t.1.y, left_t.1.z, left_t.1.w, left_t.2.x, left_t.2.y, left_t.2.z, left_t.2.w, left_t.3.x, left_t.3.y, left_t.3.z, left_t.3.w] as [Any]
-            var dataString: [String] = [String(frameId), time]
-            for i in 0..<data.count {
-                dataString.append(String(format: "%.8f", data[i] as! CVarArg))
-            }
-            //print(dataString)
-            dataLists.append(dataString)
-        }
-    }
-    
-    override func updateViewWithScene(withFaceAnchor: ARFaceAnchor){
-        if(withFaceAnchor.isTracked){
-            errorLabel.isHidden = true
-        }else{
-            errorLabel.isHidden = false
         }
     }
 }
